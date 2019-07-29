0660e94ae04464ea568638c1cb3b2115060b46bb
breakinferno <1972952841@qq.com>
2019-07-29 09:34:15 +0800

    chore: 修改配置文件

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,12 @@ WORKDIR $GOPATH/src/github.com/sundogrd/comment-grpc
 ENV GO111MODULE=on
 ENV GOFLAGS=-mod=vendor
 
+ARG DB_USER
+ARG DB_PWD
 
 RUN go build .
 
-RUN "./devops/build_docker.sh"
+RUN "./devops/build_docker.sh" $DB_USER $DB_PWD
 
 EXPOSE 24952
   
--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,8 +2,15 @@
 
 git pull
 
-docker pull breakinferno/comment-grpc:$1-$2
+
+# 删除原有镜像和容器
+if docker images | grep -q  breakinferno/comment-grpc; then
+    docker rmi -f `docker images | grep breakinferno/comment-grpc | awk '{print $3}'`
+fi
+
 if docker ps -a | grep -q sundogrd-comment-grpc; then
     docker rm -f $(docker ps -a | grep sundogrd-comment-grpc | awk '{print $1}')
 fi
+
+docker pull breakinferno/comment-grpc:$1-$2
 docker run -d --name sundogrd-comment-grpc -p 24592:24592 breakinferno/comment-grpc:$1-$2
