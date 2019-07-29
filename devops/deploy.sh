ff101f0e8f873bad14e5cbe027537881b128d438
breakinferno <1972952841@qq.com>
2019-07-29 13:13:11 +0800

    chore: 修改配置文件

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -4,12 +4,12 @@ git pull
 
 
 # 删除原有镜像和容器
-if docker images | grep -q  breakinferno/comment-grpc; then
-    docker rmi -f `docker images | grep breakinferno/comment-grpc | awk '{print $3}'`
-fi
-
 if docker ps -a | grep -q sundogrd-comment-grpc; then
     docker rm -f $(docker ps -a | grep sundogrd-comment-grpc | awk '{print $1}')
+fi
+
+if docker images | grep -q  breakinferno/comment-grpc; then
+    docker rmi -f `docker images | grep breakinferno/comment-grpc | awk '{print $3}'`
 fi
 
 docker pull breakinferno/comment-grpc:$1-$2
