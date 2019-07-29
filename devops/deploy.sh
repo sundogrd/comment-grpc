df623dbf61aba55408ab22a7d716a9758302aa20
lwyj123 <liang.pearce@gmail.com>
2019-07-29 22:26:08 +0800

    feat: change docker hub

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -8,9 +8,9 @@ if docker ps -a | grep -q sundogrd-comment-grpc; then
     docker rm -f $(docker ps -a | grep sundogrd-comment-grpc | awk '{print $1}')
 fi
 
-if docker images | grep -q  breakinferno/comment-grpc; then
-    docker rmi -f `docker images | grep breakinferno/comment-grpc | awk '{print $3}'`
+if docker images | grep -q  sundogrd/comment-grpc; then
+    docker rmi -f `docker images | grep sundogrd/comment-grpc | awk '{print $3}'`
 fi
 
-docker pull breakinferno/comment-grpc:$1-$2
-docker run -d --name sundogrd-comment-grpc -p 24592:24592 breakinferno/comment-grpc:$1-$2
+docker pull sundogrd/comment-grpc:$1-$2
+docker run -d --name sundogrd-comment-grpc -p 24592:24592 sundogrd/comment-grpc:$1-$2
